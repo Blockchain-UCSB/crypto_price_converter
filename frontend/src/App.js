cfd580d51475aa77b8eb3bbbb433082f44e6cd1f
allyhan111 <allyhan111@gmail.com>
2023-11-30 19:26:07 -0800

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import CategoryDropdown from './CategoryDropdown';
 import ChakraBoxExample from './ChakraBoxExample';
+import SimpleComponent from './title';
+import WithSubnavigation from './navbar ';
 function App() {
   return (
     <div className="App">
       <CategoryDropdown />
       <CategoryDropdown />
       <ChakraBoxExample />
+      <SimpleComponent></SimpleComponent>
+      <WithSubnavigation></WithSubnavigation>
     </div>
   );
 }
